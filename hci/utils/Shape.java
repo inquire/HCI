f1f6e6ff42d1d4e22611f0e6003a425018a59888
inquire <stanoescu.dan@gmail.com>
2011-10-02 11:50:48 +0100

    Adding a higher order abstraction class for polygons.

---
+++ hci/utils/Shape.java
@@ -0,0 +1,12 @@
+package hci.utils;
+
+import java.util.ArrayList;
+//import java.awt.Color;
+//import hci.utils.Point;
+import hci.utils.Polygon;
+
+public class Shape {
+
+	public ArrayList<Polygon> shapes = null;
+		
+}
