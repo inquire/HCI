0e515030905ede4235fb9d5289bda8036d10da0f
inquire <stanoescu.dan@gmail.com>
2011-10-16 20:06:32 +0100

    Added polygon modifier.

--- hci/utils/CustomImage.java
+++ hci/utils/CustomImage.java
@@ -1,11 +1,12 @@
 package hci.utils;
 
+import java.awt.Polygon;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import hci.utils.Polygon;
+import hci.utils.BetaPolygon;
 
-public class CustomImage implements Iterable<Polygon>, java.io.Serializable{
+public class CustomImage implements Iterable<BetaPolygon>, java.io.Serializable{
 
 	/**
 	 * @Author Daniel Stanoescu
@@ -13,21 +14,25 @@ public class CustomImage implements Iterable<Polygon>, java.io.Serializable{
 	 * Class containing a set of polygons which are comprised of multiple points.
 	 */
 	private static final long serialVersionUID = 1L;
-	private ArrayList<Polygon> shapes = null;
+	private ArrayList<BetaPolygon> shapes = null;
 	private String fileName;
 	
 	public CustomImage(){
-		shapes = new ArrayList<Polygon>();
+		shapes = new ArrayList<BetaPolygon>();
 	}
 	
-	public void addPolygon(Polygon polygon){
+	public void addPolygon(BetaPolygon polygon){
 		shapes.add(polygon);
 	}
 	
-	public Polygon getPolygon(int position){
+	public BetaPolygon getPolygon(int position){
 		return shapes.get(position);
 	}
 	
+	public void removePolygon(int position){
+		shapes.remove(position);
+	}
+		
 	public int size(){
 		return shapes.size();
 	}
@@ -45,8 +50,15 @@ public class CustomImage implements Iterable<Polygon>, java.io.Serializable{
 		shapes.clear();
 	}
 	
+	public void modify(Polygon area, BetaPolygon polygon){
+		int location = shapes.indexOf(polygon);
+		shapes.get(location).translate(area);
+	}
+	
+	
+	
 	@Override
-	public Iterator<Polygon> iterator(){
+	public Iterator<BetaPolygon> iterator(){
 		return shapes.iterator();
 	}
 		
